0edac37a9248f18c8fe01b1b7b86c8a23ef3e56d
zihanjian <zihanjian@bytedance.com>
2025-06-09 00:36:08 +0800

    添加@vercel/analytics依赖并在应用程序组件中集成分析功能，优化依赖管理

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 } 
